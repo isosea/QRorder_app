ecaf58076e094bb1f088f01da07048e3f73b70c4
isosea <isokazu.psy@gmail.com>
2021-03-29 23:04:08 +0900

    Generate menu model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_053234) do
+ActiveRecord::Schema.define(version: 2021_03_29_135743) do
+
+  create_table "menus", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
