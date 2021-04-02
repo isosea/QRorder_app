602df5545ce7b9c459a0bb012cf13df88537a0af
isosea <isokazu.psy@gmail.com>
2021-04-02 15:33:33 +0900

    Improve view

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,17 @@
 class MenusController < ApplicationController
+
  def index
+ end
+
+ def show
+ end
+
+ def confirm
+ end
+
+ def history
+ end
+
+ def check
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'menus#index'
+  get  '/show',    to: 'menus#show'
+  get  '/confirm', to: 'menus#confirm'
+  get  '/history', to: 'menus#history'
+  get  '/check',   to: 'menus#check'
   resources :users
 end
