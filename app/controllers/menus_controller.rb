b28a348c1b7f883a25ad620fdb63f38534e10ec7
isosea <isokazu.psy@gmail.com>
2021-04-13 20:07:55 +0900

    Set up user

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,5 @@
 class MenusController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :destroy]
 
  def index
   @menu1 = Menu.find_by(id: 1)
@@ -20,4 +21,30 @@ class MenusController < ApplicationController
 
  def check
  end
+
+ def new
+  @menu = Menu.new if logged_in?
+ end
+
+ def create
+  @menu = Menu.new(menu_params)
+    if @menu.save
+      flash[:success] = "Menu item created!"
+      redirect_to root_url
+    else
+      render 'menus/new'
+    end
+ end
+
+ def destroy
+  @menu.destroy
+  flash[:success] = "Menu deleted"
+  redirect_to request.referrer || root_url
+ end
+
+ private
+
+  def menu_params
+    params.require(:menu).permit(:content, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root 'menus#index'
-  get  '/show',    to: 'menus#show'
-  get  '/confirm', to: 'menus#confirm'
-  get  '/history', to: 'menus#history'
-  get  '/check',   to: 'menus#check'
+  root   'menus#index'
+  get    '/show',    to: 'menus#show'
+  get    '/confirm', to: 'menus#confirm'
+  get    '/history', to: 'menus#history'
+  get    '/check',   to: 'menus#check'
+  get    '/new',     to: 'menus#new'
+  post   '/menus',   to: 'menus#create'
+  delete '/menus',   to: 'menus#destroy'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
